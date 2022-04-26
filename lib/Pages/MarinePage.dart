9fdc621f67e0adc60e68ad0e3e9aa0fb3ea64c2e
sinan <msinan058>
2022-04-27 00:16:06 +0300

    Map Added

--- lib/Pages/MarinePage.dart
+++ lib/Pages/MarinePage.dart
@@ -1,6 +1,7 @@
-// ignore_for_file: file_names
+// ignore_for_file: file_names, prefer_collection_literals
 
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MarinePage extends StatefulWidget {
   const MarinePage({Key? key}) : super(key: key);
@@ -10,10 +11,24 @@ class MarinePage extends StatefulWidget {
 }
 
 class _MarinePageState extends State<MarinePage> {
+  LatLng laundaPozition = const LatLng(-8.808333, 13.234444);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("marine"),
+    return Center(
+      child: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: CameraPosition(target: laundaPozition, zoom: 13),
+        markers: createMarker(),
+      ),
     );
+  }
+
+  Set<Marker> createMarker() {
+    return <Marker>[
+      Marker(
+        markerId: const MarkerId("marker"),
+        position: laundaPozition,
+      )
+    ].toSet();
   }
 }
